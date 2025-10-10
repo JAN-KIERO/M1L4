f4912ac2a46b65df644125e5849a9a4d2b5ae5c0
JANEK-KIERAI <jangorgorjan@gmail.com>
2025-10-10 20:56:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from generator import gen
+from young import you
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -28,7 +30,24 @@ async def hello(ctx):
     await ctx.send(f'Cześć, jestem bot{bot.user}!')
 
 @bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+async def spam(ctx, yourword = "", count_word = 5):
+    await ctx.send(yourword * count_word)
 
+
+@bot.command()
+async def word(ctx, wordls = ""):
+    await ctx.send(you(wordls))
+    
+@bot.command()
+async def helpbot(ctx):
+    await ctx.send("❓💬 JAK OBSŁUGIWAĆ CHATBOTA 💬❓")
+    await ctx.send("📌 - Jeśli edytujesz wiadomość, CHATBOT przyśle wiadomość przed edycją 📝")
+    await ctx.send("🔑 - Jeśli wpiszesz $password (długość hasła)/jeśli nie wpiszesz liczby, hasło będzie miało 8 znaków 🔐")
+    await ctx.send("👋 - Jeśli wpiszesz $hello, bot się z tobą przywita 🤖")
+    await ctx.send("⚡ - Jeśli wpiszesz $spam (słowo które ma być powtórzone) (liczba powtórzeń/jeśli nie wpiszesz liczby, automatycznie zostanie wybrana liczba 5) 🌀")
+    await ctx.send("📖 - Jeśli wpiszesz $word (któreś z tych słów: CRINGE, LOL, ROFL, SHEESH, CREEPY), CHATBOT napisze definicję tych słów 🧐")
+
+
+
+    
 bot.run("")
